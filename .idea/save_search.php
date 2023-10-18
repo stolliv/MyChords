8de8dbeb883509d10d454e12218a64b08bcd9406
Stenzel <olivier.stenzel@web.de>
2023-10-18 22:26:59 +0200

    added search.csv

--- .idea/save_search.php
+++ .idea/save_search.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $data = json_decode(file_get_contents("php://input"), true);
 $newSearch = $data["searchValue"];
 $file = fopen("search.csv", "a");
--- .idea/script.js
+++ .idea/script.js
@@ -1,47 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Load favorites from the server when the page loads
-  loadFavorites();
-
-  // Load recent searches from localStorage
-  loadRecentSearches();
-});
-
-function searchSong() {
+async function searchSong() {
   const searchValue = document.getElementById("searchInput").value;
   const targetUrl = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + encodeURIComponent(searchValue);
-
-  // Save to recent searches
-  saveRecentSearch(searchValue);
-
   window.open(targetUrl, '_blank');
-}
 
-function loadRecentSearches() {
-  // Implement logic to load recent searches from localStorage
-  // and populate the "recentSearches" select element
-}
-
-function saveRecentSearch(searchValue) {
-  // Implement logic to save the search to localStorage
-}
-
-function loadFavorites() {
-  fetch('/load_favorites.php')
-    .then(response => response.json())
-    .then(data => {
-      // Populate the "favorites" div
-    });
-}
-
-function addFavorite(title) {
-  fetch('/add_favorite.php', {
+  // Save recent search
+  await fetch('save_search.php', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ title })
-  })
-  .then(() => {
-    loadFavorites();
+    body: JSON.stringify({searchValue})
   });
+  loadRecentSearches();
+}
+
+async function loadFavorites() {
+  const res = await fetch('load_favorites.php');
+  const data = await res.json();
+  const favoritesList = document.getElementById('favoritesList');
+  favoritesList.innerHTML = data.map(title => `<div onclick="searchSpecificSong('${title}')">${title}</div>`).join('');
+}
+
+async function loadRecentSearches() {
+  const res = await fetch('load_searches.php');
+  const data = await res.json();
+  const recentSearches = document.getElementById('recentSearches');
+  recentSearches.innerHTML = data.map(title => `<option>${title}</option>`).join('');
+}
+
+function searchSpecificSong(title) {
+  const targetUrl = "https://www.ultimate-guitar.com/search.php?search_type=title&value=" + encodeURIComponent(title);
+  window.open(targetUrl, '_blank');
 }
